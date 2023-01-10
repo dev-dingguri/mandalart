fd16c8426e2d8ec29aa9b824536c9fc4a21632e0
dev-dingguri <100068248+dev-dingguri@users.noreply.github.com>
2023-01-11 07:27:01 +0900

    [완료] 국제화

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,21 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import detector from 'i18next-browser-languagedetector';
-import * as resources from './resources';
+import * as languages from './languages';
 
 i18n
   .use(detector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
-    supportedLngs: Object.keys(resources),
+    supportedLngs: Object.keys(languages),
     detection: {
       order: ['path', 'navigator'],
       lookupFromPathIndex: 0,
     },
     keySeparator: '.',
     load: 'languageOnly',
-    resources,
+    resources: languages,
   });
 
 export default i18n;
