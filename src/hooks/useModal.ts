1270cee48708acb3ad986ec8f2d1e8a2397dcfc7
dev-dingguri <dev.dingguri@gmail.com>
2023-09-25 08:22:29 +0900

    fix: 모달이 닫힐 때 내용이 제거되는게 보이는 오류 수정

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -9,8 +9,9 @@ const useModal = <T extends ReactNode = ReactNode>() => {
     setIsOpen(true);
   }, []);
   const close = useCallback(() => {
-    setContent(null);
     setIsOpen(false);
+    // 모달이 닫힐 때 애니메이션이 있는 경우가 있어서 닫힐 때 내용을 제거하지 않음
+    // setContent(null);
   }, []);
 
   return {
