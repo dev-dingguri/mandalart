eba8453afb0a449a73af09f173c9b249c6d7be49
dev-dingguri <dev.dingguri@gmail.com>
2026-03-14 23:37:02 +0900

    fix(dropdown): DropdownMenu Positioner에 pointer-events-auto 추가

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -31,7 +31,7 @@ function DropdownMenuContent({
   return (
     <MenuPrimitive.Portal>
       <MenuPrimitive.Positioner
-        className="isolate z-50 outline-none"
+        className="pointer-events-auto isolate z-50 outline-none"
         align={align}
         alignOffset={alignOffset}
         side={side}
